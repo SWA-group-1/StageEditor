cf9ac76998d50c83f5341bdafa111da559843c48
le0-0 <leonard.schoyen@gmail.com>
2021-04-13 19:47:06 +0200

    Fixed load_image.

--- scripts/load_image.js
+++ scripts/load_image.js
@@ -10,11 +10,14 @@ function change_image() {
   }
 
   let reader = new FileReader();
-  let url = reader.readAsDataURL(image_file_element.files[0]);
+  reader.readAsDataURL(image_file_element.files[0]);
 
-  let grid_background_element = document.querySelectorAll("#grid_background")[0];
-  grid_background_element.src = url;
-  grid_background_element.style.opacity = "1.0";
+  reader.addEventListener("load", function () {
+    let grid_background_element = document.querySelectorAll("#grid_background")[0];
+    grid_background_element.src = reader.result;
+    grid_background_element.style.opacity = "1.0";
+    grid_background_element.style.padding = "0";
+  });
 }
 
 window.addEventListener("load", add_image_file_change_listener);
