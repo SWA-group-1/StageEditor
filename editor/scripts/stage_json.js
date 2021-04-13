30d1a24ecc0d5acb1fc5462ce016975864ed04c1
le0-0 <leonard.schoyen@gmail.com>
2021-04-13 23:04:23 +0200

    Project restructure and adding stages folder.

---
+++ editor/scripts/stage_json.js
@@ -0,0 +1,39 @@
+function update_json() {
+  let grid_element = document.querySelectorAll("#grid")[0];
+  let stage_json_element = document.querySelectorAll("#stage_json")[0];
+  let column_number = Math.round(document.querySelectorAll("#column_number")[0].value);
+  let row_number = Math.round(document.querySelectorAll("#row_number")[0].value);
+
+  let text = `{\n`;
+
+  text += `\t"Number": ${Math.round(document.querySelectorAll("#number")[0].value)},\n`;
+  text += `\t"Name": "${document.querySelectorAll("#name")[0].value}",\n`;
+  text += `\t"XSize": "${column_number}",\n`;
+  text += `\t"YSize": "${row_number}",\n`;
+
+  text += `\t"BlockedTiles": [\n`;
+  let tiles = grid_element.tiles;
+  let contains_blocked_tiles = false;
+  for (let i = 0; i < tiles.length; i++) {
+    if (!tiles[i].classList.contains("blocked")) {
+      continue;
+    }
+
+    contains_blocked_tiles = true;
+    let x = i % column_number;
+    let y = (i - x) / column_number;
+    text += `\t\t{ "X": ${Math.round(x)}, "Y": ${Math.round(y)} },\n`;
+  }
+  if (contains_blocked_tiles) {
+    text = text.substring(0, text.length - 2);
+  }
+  text += `\n\t],\n`;
+  text += `}`;
+
+  stage_json_element.innerText = text;
+}
+
+window.addEventListener("load", function () {
+  document.querySelectorAll("#number")[0].addEventListener("change", update_json);
+  document.querySelectorAll("#name")[0].addEventListener("change", update_json);
+});
